ca9351a96fa83bf74cb8c0d025e9953cd85efad3
chenrui <1040050969@qq.com>
2019-04-24 17:27:12 +0800

    test

--- src/main/java/com/example/demo/controller/Test.java
+++ src/main/java/com/example/demo/controller/Test.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Test {
     @GetMapping(value = "/fun")
     public String function(){
-        return "good luck!!!!!!!!";
+        return "good luck chenrui";
     }
 }
